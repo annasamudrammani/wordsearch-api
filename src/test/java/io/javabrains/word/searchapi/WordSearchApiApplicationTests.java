385c46cf5205a97d69f61583cc19422ba4ac4bc1
Nagamani Annasamudram <manikashvi@Nagamanis-MBP.lan>
2022-01-08 11:38:05 -0500

    Wordsearch API implementation

---
+++ src/test/java/io/javabrains/word/searchapi/WordSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.word.searchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
